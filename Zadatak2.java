348a086eb6dd5085225f8621692f75a1175bffda
Mirko Resetar <reso28@gmail.com>
2020-01-08 11:49:57 +0100

    Create Zadatak2.java

---
+++ Zadatak2.java
@@ -0,0 +1,21 @@
+
+public class Zadatak2 {
+	
+	
+	
+	
+	
+public static void main(String[] args) {
+	
+	
+	for(int x=0;x<15;x++) {
+		
+		for(int y=0;y<15;y++) {
+			System.out.print((x+1)*(y+1) + "\t ");
+		}
+		System.out.println();
+	}
+	
+}
+
+}
